db04e9a9a085538f5cfe4982e16c6eaecb84706f
leonardo cintio <corduroy76@gmail.com>
2013-07-14 17:48:24 +0200

    Int values bug fixing + added sample file

--- json2csv.py
+++ json2csv.py
@@ -4,10 +4,11 @@ import simplejson as json
 import csv
 import cStringIO
 import os
+from types import *
 
 class Json2csvCommand(sublime_plugin.TextCommand):
 
-	# 1 - get the content of the json file or selection
+	# Get the content of the json file or selection
 	def get_json_content(self):
 		regions = self.view.sel()
 		if len(regions) > 0 and not regions[0].empty():
@@ -17,7 +18,7 @@ class Json2csvCommand(sublime_plugin.TextCommand):
 
 		return self.view.substr(selection)
 
-	# 2 - write data to csv
+	# Write data to csv
 	def write_csv(self, csv_buffer):
 		write_header = True
 		item_keys    = []
@@ -30,7 +31,11 @@ class Json2csvCommand(sublime_plugin.TextCommand):
 				if write_header:
 					item_keys.append(key)
 
-				item_values.append(item[key].encode('utf-8'))
+				value = item.get(key, '')
+				if type(value) is StringTypes:
+					item_values.append(value.encode('utf-8'))
+				else:
+					item_values.append(value)
 
 			if write_header:
 				writer.writerow(item_keys)
@@ -41,13 +46,13 @@ class Json2csvCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		self.data            = []
 		self.source_filename = self.view.file_name()
-		print self.source_filename
+		#print self.source_filename
 
 		json_content = self.get_json_content()
 
-		# validate json data
+		# Validate json data
 		try:
-			print "json data is valid"
+			#print "json data is valid"
 			self.data = json.loads(json_content)
 			
 			if self.source_filename:
@@ -63,7 +68,7 @@ class Json2csvCommand(sublime_plugin.TextCommand):
 				window = self.view.window()
 				csv_file_view = window.new_file()
 				selection = sublime.Region(0, csv_file_view.size())
-				print selection
+				#print selection
 
 				edit = csv_file_view.begin_edit('json2csv')
 				csv_file_view.replace(edit, selection, csv_buffer.getvalue())
@@ -73,4 +78,4 @@ class Json2csvCommand(sublime_plugin.TextCommand):
 			import sys
 			exc = sys.exc_info()[1]
 			sublime.status_message(str(exc))
-			print str(exc)
+			#print str(exc)
